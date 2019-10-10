fe50a1843ac8bad606a4f3a05e219a61a7341660
Steven Danesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-10-10 07:54:48 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan18/kelipatan/PBO2_10118078_Latihan18_Kelipatan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama     : Steven Danesswaralay
+ * Kelas    : PBO2
+ * Nim      : 10118078
+ * Deskripsi Program : Kelipatan
+
+ */
+public class PBO2_10118078_Latihan18_Kelipatan {
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);
+        }
+    }
+}
